9da79c5c6b5944e6b2d1e9c13697f2c5319d1ee6
Quanxing Liu <liuqx0717@gmail.com>
2020-10-31 14:51:41 -0400

    Added tee.h

--- src/hw4-utils.h
+++ src/hw4-utils.h
@@ -5,6 +5,8 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/BasicBlock.h"
 
+#include "tee.h"
+
 #include <fstream>
 #include <iostream>
 #include <type_traits>
@@ -15,39 +17,27 @@ public:
   enum class EdgeType {Default, T, F};
 
 private:
-  std::ostream &out;
-  std::ofstream file;
-
-  // Support BasicBlock, Function, ...
-  template<typename T>
-  void printEdge(std::ostream &stream, const T &from, const T &to, 
-                 EdgeType type = EdgeType::Default) {
-    static_assert(std::is_pointer<T>::value == false, "Don't pass a pointer to printEdge()");
-    stream << "edge: " << from.getName().str();
-    if (type == EdgeType::T) stream << "(T)";
-    else if (type == EdgeType::F) stream << "(F)";
-    stream << " ---> " << to.getName().str() << std::endl;
-  }
-
-  void printKeyBlock(std::ostream &stream, const llvm::BasicBlock &BB) {
-    stream << "Key Block: " << BB.getName().str() << std::endl;
-  }
+  std::fstream file;
+  Tee<std::fstream&, std::ostream&> tee;
 
 public:
   OFile(const llvm::Function &F):
     file(F.getName().str() + ".txt", std::ios::out | std::ios::trunc),
-    out(std::cout) {}
+    tee(file, std::cout) {
+    tee << "Function: " << F.getName().str() << "\n";
+  }
   
   // Support BasicBlock, Function, ...
   template<typename T>
-  void printEdge(const T &from, const T &to,
-                 EdgeType type = EdgeType::Default) {
-    printEdge(out, from, to, type);
-    printEdge(file, from, to, type);
+  void printEdge(const T &from, const T &to, EdgeType type = EdgeType::Default) {
+    static_assert(std::is_pointer<T>::value == false, "Don't pass a pointer to printEdge()");
+    tee << "edge: " << from.getName().str();
+    if (type == EdgeType::T) tee << "(T)";
+    else if (type == EdgeType::F) tee << "(F)";
+    tee << " ---> " << to.getName().str() << "\n";
   }
 
   void printKeyBlock(const llvm::BasicBlock &BB) {
-    printKeyBlock(out, BB);
-    printKeyBlock(file, BB);
+    tee << "Key Block: " << BB.getName().str() << "\n";
   }
 };
